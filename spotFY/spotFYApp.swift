4258d39a81fcda250ce5f8edfc99efc751175196
davidAg9 <shane_alpha9@outlook.com>
2022-03-11 08:07:49 +0000

    Initial Commit

---
+++ spotFY/spotFYApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotFYApp.swift
+//  spotFY
+//
+//  Created by Dave Agyakwa on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct spotFYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
